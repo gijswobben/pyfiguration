8fb5ad5bd9120c8b1e734c6fc31dd7dbc1098e1a
Gijs Wobben <gijswobben@gmail.com>
2020-06-25 09:37:18 +0200

    Updated info for PyPi (Python versions supported)

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ setuptools.setup(
         "pyyaml"
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
