03a914105498ed310b2cad266f92ff4111ae274d
Gijs Wobben <gijswobben@gmail.com>
2020-06-25 09:38:54 +0200

    Removed support for 3.9 (not supported by the test pipeline)

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
