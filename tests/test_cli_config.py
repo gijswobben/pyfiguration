3068147583ad342459af075df154a3c399ffc2f8
Gijs Wobben <gijswobben@gmail.com>
2020-07-06 10:03:08 +0200

    Improved test coverage of the CLI

--- tests/test_cli_config.py
+++ tests/test_cli_config.py
@@ -1,10 +1,22 @@
 import io
 
-from pyfiguration.cli import cli
-from contextlib import redirect_stdout
+from pyfiguration.cli import cli, inspectConfig
+from contextlib import redirect_stderr, redirect_stdout
 
 
-def test_cli_module():
+def test_inspectConfig():
+    f = io.StringIO()
+    with redirect_stdout(f):
+        inspectConfig(
+            "./examples/simple/script.py",
+            sources=["./examples/simple/config_with_warnings.yaml"],
+        )
+    result = f.getvalue()
+    assert "✗" in result
+    assert "!" in result
+
+
+def test_cliInspectConfig():
     f = io.StringIO()
     with redirect_stdout(f):
         cli(
